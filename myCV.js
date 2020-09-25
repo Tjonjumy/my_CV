92fd430f43b71c0bf16098744175ae3e6d99a95f
tjonjumy <tjonjumy@gmail.com>
2020-09-25 15:26:31 +0700

    Initial commit

---
+++ myCV.js
@@ -0,0 +1,31 @@
+//Initial Intro
+// window.onload=function(){
+//   introElements=document.getElementsByClassName('intro');
+//   introElements=Array.from(introElements);
+//   console.log(introElements.length);
+//   var timeDelay=1000;
+//   for(let i=0;i< introElements.length;i++){
+//     setTimeout(function(){console.log(introElements[i]);introElements[i].style.top="0";console.log(timeDelay);},timeDelay); 
+//     timeDelay+=500;
+//   }
+//   // for(let i=0;i< introElements.length;i++){
+//   //   if()
+//   //   introElements[i].style.display="none"; 
+//   // }
+// };
+//Make Circle text
+function circularText(txt, radius, classIndex) {
+    txt = txt.split(""),
+    classIndex = document.getElementsByClassName("circTxt")[classIndex];
+    var deg = 360 / txt.length,
+      origin = -90;
+  
+    txt.forEach((ea) => {
+      ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+      classIndex.innerHTML += ea;
+      origin += deg;
+    });
+    console.log(txt);
+  }
+
+    circularText("ToTop-TuanPham-", 35, 0);
